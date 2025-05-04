646d82f6be61c10c9b0660a3fa8772ec2516ede8
Cong Deng <iceage723@gmail.com>
2025-05-04 20:16:08 +0930

    fix some missing code errors

--- sr.c
+++ sr.c
@@ -138,8 +138,7 @@ void A_output(struct msg message)
 */
 void A_input(struct pkt packet)
 {
-  int ackcount = 0;
-  int i;
+  int index;
 
   /* if received ACK is not corrupted */
   if (!IsCorrupted(packet))
@@ -148,67 +147,86 @@ void A_input(struct pkt packet)
       printf("----A: uncorrupted ACK %d is received\n", packet.acknum);
     total_ACKs_received++;
 
-    /* check if new ACK or duplicate */
-    if (windowcount != 0)
+    /* check if ACK is within current window */
+    if (((windowbase <= (windowbase + windowcount - 1) % SEQSPACE) &&
+         (packet.acknum >= windowbase && packet.acknum <= (windowbase + windowcount - 1) % SEQSPACE)) ||
+        ((windowbase > (windowbase + windowcount - 1) % SEQSPACE) &&
+         (packet.acknum >= windowbase || packet.acknum <= (windowbase + windowcount - 1) % SEQSPACE)))
     {
-      int seqfirst = buffer[windowfirst].seqnum;
-      int seqlast = buffer[windowlast].seqnum;
-      /* check case when seqnum has and hasn't wrapped */
-      if (((seqfirst <= seqlast) && (packet.acknum >= seqfirst && packet.acknum <= seqlast)) ||
-          ((seqfirst > seqlast) && (packet.acknum >= seqfirst || packet.acknum <= seqlast)))
-      {
 
-        /* packet is a new ACK */
+      index = packet.acknum % WINDOWSIZE;
+
+      /* Only process if not already ACKed */
+      if (!acked[index])
+      {
         if (TRACE > 0)
           printf("----A: ACK %d is not a duplicate\n", packet.acknum);
         new_ACKs++;
 
-        /* cumulative acknowledgement - determine how many packets are ACKed */
-        if (packet.acknum >= seqfirst)
-          ackcount = packet.acknum + 1 - seqfirst;
-        else
-          ackcount = SEQSPACE - seqfirst + packet.acknum;
+        /* mark as ACKed */
+        acked[index] = true;
 
-        /* slide window by the number of packets ACKed */
-        windowfirst = (windowfirst + ackcount) % WINDOWSIZE;
+        /* If this was the packet we were timing, stop timer and find next to time */
+        if (packet.acknum == oldest_unacked)
+        {
+          stoptimer(A);
+          find_oldest_unacked();
 
-        /* delete the acked packets from window buffer */
-        for (i = 0; i < ackcount; i++)
+          /* If there are still unacked packets, restart timer */
+          if (oldest_unacked != -1)
+          {
+            starttimer(A, RTT);
+            /*if (TRACE > 1)
+              printf("----A: Started timer for packet %d\n", oldest_unacked);*/
+          }
+        }
+
+        /* Slide window if base packet is ACKed */
+        while (windowcount > 0 && acked[windowbase % WINDOWSIZE])
+        {
+          windowbase = (windowbase + 1) % SEQSPACE;
           windowcount--;
-
-        /* start timer again if there are still more unacked packets in window */
-        stoptimer(A);
-        if (windowcount > 0)
-          starttimer(A, RTT);
+        }
+      }
+      else
+      {
+        if (TRACE > 0)
+          printf("----A: duplicate ACK %d, do nothing!\n", packet.acknum);
       }
     }
-    else if (TRACE > 0)
-      printf("----A: duplicate ACK received, do nothing!\n");
+    else
+    {
+      if (TRACE > 0)
+        printf("----A: ACK %d outside window, do nothing!\n", packet.acknum);
+    }
   }
-  else if (TRACE > 0)
-    printf("----A: corrupted ACK is received, do nothing!\n");
+  else
+  {
+    if (TRACE > 0)
+      printf("----A: corrupted ACK is received, do nothing!\n");
+  }
 }
 
 /* called when A's timer goes off */
 void A_timerinterrupt(void)
 {
-  int i;
+  int index;
 
   if (oldest_unacked != -1)
   {
-    i = oldest_unacked % WINDOWSIZE;
+    index = oldest_unacked % WINDOWSIZE;
 
     if (TRACE > 0)
       printf("----A: time out,resend packets!\n");
 
     /* Only resend if still within window and not yet ACKed */
-    if (!acked[i])
+    if (!acked[index])
     {
       /* resend just this packet */
       if (TRACE > 0)
         printf("---A: resending packet %d\n", oldest_unacked);
 
-      tolayer3(A, buffer[i]);
+      tolayer3(A, buffer[index]);
       packets_resent++;
 
       starttimer(A, RTT);
